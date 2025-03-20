c51e1f582e3f824f4f2046de0f089f501891176f
MICHELLE SOFIA GARBER <48521612@ort>
2025-03-20 12:11:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function confirmarContraseña() {
+    var contraseña = document.getElementById("contraseña").value;
+    var contraseña2 = document.getElementById("contraseña2").value;
+    
+    var errorMessage = document.getElementById("error-message");
+    submitBtn = document.getElementById("submit-btn");
+
+    if (contraseña === contraseña2 && contraseña.length >= 8) {
+        errorMessage.style.display = "none";
+        submitBtn.disabled = false;
+    } else {
+        errorMessage.style.display = "block"; 
+        submitBtn.disabled = true; 
+    }
+}
+
